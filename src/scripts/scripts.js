a0e7b69230d99c74e091633d261ec17251c3d0ff
Francisco Gemaque <gemaquejr@hotmail.com>
2024-12-16 15:23:01 -0300

    Finaliza a função carrossel na home page
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,6 +1,6 @@
 function handleMouseEnter() {
 	this.classList.add("s-card--hovered");
-    document.body.id = `${this.id}-hovered`;
+	document.body.id = `${this.id}-hovered`;
 }
 
 function handleMouseLeave() {
@@ -19,3 +19,23 @@ function addEventListenersToCards() {
 }
 
 document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+
+function selectCarouselItem(selectedButtonElement) {
+	const selectedItem = selectedButtonElement.id;
+	const carousel = document.querySelector(".s-cards-carousel");
+	const transform = carousel.style.transform;
+	const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+	const rotateYDeg = -120 * (Number(selectedItem) - 1);
+	const newTransform = transform.replace(
+		rotateY[0],
+		`rotateY(${rotateYDeg}deg)`,
+	);
+
+	carousel.style.transform = newTransform;
+
+	const activeButtonElement = document.querySelector(
+		".s-controller__button--active",
+	);
+	activeButtonElement.classList.remove("s-controller__button--active");
+	selectedButtonElement.classList.add("s-controller__button--active");
+}
